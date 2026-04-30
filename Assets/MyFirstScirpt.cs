d813fd1ce1fcbdfe24ac8fa49954016e9512549e
jadeeu <S10273501@connect.np.edu.sg>
2026-04-30 13:53:44 +0800

    newposition

--- Assets/MyFirstScirpt.cs
+++ Assets/MyFirstScirpt.cs
@@ -2,9 +2,11 @@
 
 public class MyFirstScirpt : MonoBehaviour
 {
+     Vector3 newPosition = new Vector3(1.0f,2.0f,3.0f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        transform.position = newPosition;
         print(transform.position);
     }
 
