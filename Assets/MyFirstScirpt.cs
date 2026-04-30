22a555c588195906433690a0919e48e7666b1709
jadeeu <S10273501@connect.np.edu.sg>
2026-04-30 14:29:44 +0800

    move_continue

--- Assets/MyFirstScirpt.cs
+++ Assets/MyFirstScirpt.cs
@@ -6,7 +6,7 @@ public class MyFirstScirpt : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position += newPosition;
+        transform.position = newPosition;
         print(transform.position);
     }
 
