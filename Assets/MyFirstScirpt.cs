8aaba5979a59d1dc22e8c08bff9b522ae8669f8b
jadeeu <S10273501@connect.np.edu.sg>
2026-04-30 15:23:44 +0800

    rotate

--- Assets/MyFirstScirpt.cs
+++ Assets/MyFirstScirpt.cs
@@ -2,26 +2,28 @@
 
 public class MyFirstScirpt : MonoBehaviour
 {
-  public float speed = 5f;
+      public float speed = 5f;
+    public float rotateSpeed = 100f;
     public float limit = 5f;
 
-    Vector3 direction = Vector3.right; // move along X
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Vector3 direction = Vector3.right;
     void Start()
     {
-      
     }
 
     // Update is called once per frame
     void Update()
-    {
-       // Move continuously
+  {
+     // movement
         transform.position += direction * speed * Time.deltaTime;
 
-        // Check if beyond limit
+        // rotation
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+
+        // reverse movement at limit
         if (transform.position.x > limit || transform.position.x < -limit)
         {
-            direction = -direction; // reverse direction
-    }
+            direction = -direction;
+   }
   }
 }
