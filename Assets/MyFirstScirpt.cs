eecc94a77a7d199b543a7cd549e3cbb9eaaf7e9d
jadeeu <S10273501@connect.np.edu.sg>
2026-05-07 13:49:27 +0800

    collision

--- Assets/MyFirstScirpt.cs
+++ Assets/MyFirstScirpt.cs
@@ -2,30 +2,19 @@
 
 public class MyFirstScirpt : MonoBehaviour
 {
-    public float speed = 100f;
+    public float speed = 2f;
     public float maxAngle = 45f;
-
-    float currentRotation = 0f;
-    float direction = 1f;
 
     Vector3 direction = Vector3.right;
     void Start()
     {
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        // rotate
-        float rotationThisFrame = speed * Time.deltaTime * direction;
-        transform.Rotate(0, rotationThisFrame, 0);
-
-        currentRotation += rotationThisFrame;
-
-        // reverse direction at limit
-        if (currentRotation > maxAngle || currentRotation < -maxAngle)
-        {
-            direction = -direction;
-        }
+        float angle = Mathf.Sin(Time.time * speed) * maxAngle;
+        transform.eulerAngles = new Vector3(0, angle, 0);
     }
 }
---
+++ Assets/scripts/collisions.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class collisions : MonoBehaviour
+{
+void OnCollisionEnter(Collision collision)
+{
+    print("Collied detected");
+}
+}
