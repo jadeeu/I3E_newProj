efeba1deb82b4ed6a8887f882f229ee97827d375
jadeeu <S10273501@connect.np.edu.sg>
2026-05-07 14:29:21 +0800

    stay

--- Assets/scripts/collisions.cs
+++ Assets/scripts/collisions.cs
@@ -5,9 +5,18 @@ public class collisions : MonoBehaviour
 void OnCollisionEnter(Collision collision)
 {
     print("Collied detected with" + collision.gameObject.name);
+    if(collision.gameObject.name == "Plane")
+        {
+            Destroy(gameObject);
+        }
 }
 void OnCollisionExit(Collision collision)
 { 
     print("Collison ended with" + collision.gameObject.name);
+    Destroy(gameObject);
 }
+void OnCollisonStay(Collision collision)
+    {
+        print("Colliding ended with" + collision.gameObject.name);
+    }
 }
