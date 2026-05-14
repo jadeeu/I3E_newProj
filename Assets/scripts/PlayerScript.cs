0f71b0ebe99950e445897010a28f5e59a920eb91
jadeeu <S10273501@connect.np.edu.sg>
2026-05-14 14:04:55 +0800

    collect

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -3,34 +3,22 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     GameObject currentCollectable;
+
     int collCount = 0;
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Collectible"))
+        if(collision.gameObject.tag == "Collectible")
         {
             currentCollectable = collision.gameObject;
         }
     }
 
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Collectible"))
-        {
-            currentCollectable = null;
-        }
-    }
-
-    // This gets called by Starter Assets Input System
     void OnInteract()
     {
-        if (currentCollectable != null)
-        {
-            collCount++;
-            print("Player has collected " + collCount + " collectibles");
-
-            Destroy(currentCollectable);
-            currentCollectable = null;
-        }
+        collCount++;
+        print("Player has collected" + collCount + "collectibles");
+        Destroy(currentCollectable);
     }
+
 }
