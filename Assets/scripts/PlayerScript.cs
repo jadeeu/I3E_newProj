29020db199b2bfc0e2de712131c250f870630845
jadeeu <S10273501@connect.np.edu.sg>
2026-05-18 11:14:14 +0800

    collecting_unique

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -3,45 +3,46 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     GameObject currentCollectable;
-    [SerializeField] private float interactDistance = 5f;
-    [SerializeField] private LayerMask collectibleLayer;
-private int currentScore = 0;
 
-    int collCount = 0;
+    int totalScore = 0;
 
-    void OnCollisionEnter(Collision collision)
+    public float collectDistance = 5f;
+
+    void Update()
     {
-        if (collision.gameObject.CompareTag("Collectible"))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            currentCollectable = collision.gameObject;
+            OnInteract();
         }
     }
 
     void OnInteract()
     {
-        if (currentCollectable)
-        {
-            collCount++;
-            print("Player has collected " + collCount + " Collectibles");
-            Destroy(currentCollectable);
-        }
-    }
+        Ray ray = Camera.main.ScreenPointToRay(
+            new Vector3(Screen.width / 2, Screen.height / 2)
+        );
 
-[cite_start]public void OnInteract() // This automatically matches your 'Interact' Action [cite: 114, 127]
-{
-    RaycastHit hit;
-    [cite_start]// Shoots a ray forward from the camera [cite: 135]
-    if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, interactDistance, collectibleLayer))
-    {
-        [cite_start]// Look for your 'Collect' script on the object we hit [cite: 141]
-        Collect collectibleItem = hit.collider.GetComponent<Collect>();
-        if (collectibleItem != null)
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, collectDistance))
         {
-            [cite_start]// Get its unique score, add it, and destroy the object [cite: 142]
-            currentScore += collectibleItem.GetScoreValue();
-            Debug.Log("Current Score: " + currentScore);
-            Destroy(hit.collider.gameObject);
+            if (hit.collider.CompareTag("Collectible"))
+            {
+                currentCollectable = hit.collider.gameObject;
+
+                Collectible collectible =
+                    currentCollectable.GetComponent<Collectible>();
+
+                if (collectible != null)
+                {
+                    totalScore += collectible.score;
+
+                    print("Collected! Total Score: " + totalScore);
+                }
+
+                Destroy(currentCollectable);
+                currentCollectable = null;
+            }
         }
     }
-}
 }
--- Assets/scripts/collect.cs
+++ Assets/scripts/collect.cs
@@ -1,54 +1,6 @@
 using UnityEngine;
 
-public class collect : MonoBehaviour
+public class Collectible : MonoBehaviour
 {
-    public int collectedItems = 0;
-    public int totalCollectibles = 5;
-    [SerializeField] private int scoreValue = 10;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Collectible"))
-        {
-            collectedItems++;
-            Destroy(collision.gameObject);
-
-            Debug.Log("Collected: " + collectedItems);
-        }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("FinishZone"))
-        {
-            if (collectedItems >= totalCollectibles)
-            {
-                Debug.Log("ALL COLLECTED! LEVEL COMPLETE!");
-            }
-            else
-            {
-                Debug.Log("Not all collectibles collected yet!");
-            }
-        }
-    }
-
-    void OnInteract()
-    {
-        print("Player wants to interact with something");
-    }
-
-    public int GetScoreValue()
-{
-    return scoreValue;
-}
+    public int score = 10;
 }
