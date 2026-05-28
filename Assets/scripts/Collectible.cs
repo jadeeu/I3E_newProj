a13ef30732694b6af53b351f3c7df2736a7f4ffd
jadeeu <S10273501@connect.np.edu.sg>
2026-05-28 14:48:52 +0800

    collect

--- Assets/scripts/Collectible.cs
+++ Assets/scripts/Collectible.cs
@@ -2,22 +2,12 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    // This triggers automatically when something enters the collider
-    public void OnTriggerEnter(Collider other)
-    {
-        // Check if the object that touched us has the "Player" tag
-        if (other.CompareTag("Player"))
-        {
-            CollectItem();
-        }
-    }
+    // Add this line so PlayerScript can read how much this item is worth
+    public int score = 10; 
 
     public void CollectItem()
     {
-        // 1. Add your logic here (e.g., increase score, add to inventory)
         Debug.Log("Item Collected!");
-
-        // 2. Remove the item from the game world
         Destroy(gameObject);
     }
 }
