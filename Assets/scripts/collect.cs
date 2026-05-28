13716c190454015cb1349ef9e7fdf22d33c1036d
jadeeu <S10273501@connect.np.edu.sg>
2026-05-28 14:32:44 +0800

    audio

--- Assets/scripts/collect.cs
+++ Assets/scripts/collect.cs
@@ -4,16 +4,10 @@ public class Collect : MonoBehaviour
 {
     public int score = 10;
 
-    AudioSource collectibleAuto;
 
     public void CollectItem()
     {
         Debug.Log("Collected item worth: " + score);
         Destroy(gameObject);
-    }
-
-    void Start()
-    {
-        collectibleAudio = Getcomponent<AudioSource>();
     }
 }
