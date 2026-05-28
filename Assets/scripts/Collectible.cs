0b503a1a66c57b8fa5e10db1b3bcb20acd03d210
jadeeu <S10273501@connect.np.edu.sg>
2026-05-28 14:40:23 +0800

    collectible

--- Assets/scripts/Collectible.cs
+++ Assets/scripts/Collectible.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
-public class collectible : MonoBehaviour
+public class Collectible : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // This triggers automatically when something enters the collider
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Check if the object that touched us has the "Player" tag
+        if (other.CompareTag("Player"))
+        {
+            CollectItem();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CollectItem()
     {
-        
+        // 1. Add your logic here (e.g., increase score, add to inventory)
+        Debug.Log("Item Collected!");
+
+        // 2. Remove the item from the game world
+        Destroy(gameObject);
     }
 }
