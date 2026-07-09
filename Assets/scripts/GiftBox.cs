4f581816c52df5e5a0576cad461763a38a95d748
jadeeu <S10273501@connect.np.edu.sg>
2026-07-09 20:26:17 +0800

    testing_key

--- Assets/scripts/GiftBox.cs
+++ Assets/scripts/GiftBox.cs
@@ -8,10 +8,26 @@ public class GiftBox : MonoBehaviour
     private int pressCount = 0;
 
     void Update()
-{
-    if (Input.GetKeyDown(KeyCode.E))
     {
-        Debug.Log("E Pressed");
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E Pressed");
+        }
     }
-}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = false;
+        }
+    }
 }
