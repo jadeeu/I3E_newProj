11c828eadd255577f64cbe04a29e987cb562cc1e
jadeeu <S10273501@connect.np.edu.sg>
2026-07-11 11:52:34 +0800

    goal_script_update

--- Assets/scripts/Goal.cs
+++ Assets/scripts/Goal.cs
@@ -1,15 +1,33 @@
 using UnityEngine;
+using TMPro;
 
 public class Goal : MonoBehaviour
 {
     private int score = 0;
+
+    public TMP_Text scoreText;
+
+    void Start()
+    {
+        scoreText.text = "Score: " + score;
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
             score++;
+            scoreText.text = "Score: " + score;
+
             Debug.Log("Score: " + score);
+
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.linearVelocity = Vector3.zero;      // Unity 6
+                rb.angularVelocity = Vector3.zero;
+            }
         }
     }
 }
